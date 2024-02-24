affab3922279d6d511776de83552b4b392fc0cd4
Ramiro Cadavid <ramiro.cadavid@aflore.co>
2024-02-24 11:00:59 -0500

    Added remote Tracking Server connection

--- run.py
+++ run.py
@@ -3,7 +3,7 @@ import mlflow
 experiment_name = "ElasticNet"
 entry_point = "Training"
 
-mlflow.set_tracking_uri("http://127.0.0.1:5000")
+mlflow.set_tracking_uri("http://ec2-184-73-53-253.compute-1.amazonaws.com:5000")
 
 mlflow.projects.run(
     uri=".",

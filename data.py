20ddbba2eb6243f8cc8c2a13c656b98c259685e1
Ramiro Cadavid <ramiro.cadavid@aflore.co>
2024-02-24 19:33:32 -0500

    Fix contributor

--- data.py
+++ data.py
@@ -4,6 +4,7 @@ from sklearn.impute import KNNImputer
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import OneHotEncoder
 
+
 # Reading data
 train = pd.read_csv('data/train.csv')
 test = pd.read_csv('data/test.csv')
